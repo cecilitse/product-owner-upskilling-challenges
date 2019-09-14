a5a8e606415b78fb496d1dfd87d8f5ef07161b03
Cecile Veneziani <contact@cecilitse.org>
2019-09-14 21:45:58 +0200

    Add DB queries exercise

---
+++ 04-databases/02-sql-queries/README.md
@@ -0,0 +1,123 @@
+# SQL Queries
+
+In this exercise, we'll get used to the art of crafting SQL queries.
+
+There are way too many queries, try to go as far as you can but with no pressure since it's impossible to cover all of them.
+
+## Setup
+
+Execute the following in the Terminal:
+
+```bash
+cd ~/lewagon-workshop/04-databases/02-sql-queries/
+code .
+```
+
+## Exercise
+
+In this folder, you will find:
+
+- `team_building.sqlite` - it's the database containing all the data we want to query.
+- `queries.sql` - the expected queries
+
+You'll work in the `queries.sql` file. You'll write down all the queries you've found so far.
+
+## SQLite
+
+There are plenty of different SQL databases.
+Just to list a few, we have: SQLite, PostgreSQL, MySQL, etc.
+
+During this workshop, we'll use **SQLite** because of its simplicity of setup and usage since the database is stored within a file.
+
+Check that it is installed by typing in the Terminal:
+
+```bash
+sqlite3
+```
+
+If you see `sqlite>` then it's working.
+Otherwise ask for help.
+
+To quit, type `Ctrl + d`
+
+## DB Browser for SQLite
+
+To visualize our data in an easier way, we'll also use a software called SQLite Browser.
+
+Check that you have it installed. Otherwise, download and install it from: [https://sqlitebrowser.org](https://sqlitebrowser.org)
+
+## The database
+
+Here is the **database schema** you'll work with in this exercise:
+![Database schema](database-schema.png)
+
+And here are a few **stats** about the data inside:
+- 2 sites
+- 8 teams
+- 2 teams spread on two sites: R&D and Product Owner
+- 114 employees
+- 44 activities
+- 22 team building sessions
+- 194 participations
+
+## Objectives
+
+The goal is to query the provided `team_building.sqlite` database. You'll find the expected queries within the `queries.sql` file.
+
+How to test your queries? There are 2 options: either use the `sqlite3` command line or use the DB Browser software. See below.
+
+For each query, once you've found it, just write it down within the `queries.sql` file.
+
+### Sqlite3
+
+In the Terminal, type:
+
+```bash
+sqlite3 team_building.sqlite
+```
+
+Then enable a few options by typing:
+
+```bash
+.headers on
+.mode column
+```
+
+You can then type any SQL query.
+
+For instance, you can type:
+
+```sql
+SELECT * FROM sites;
+```
+
+To quit, type `Ctrl + d`
+
+### DB Browser for SQLite
+
+Launch the software, click on `File` > `Open Database` and select your database.
+
+As a reminder, it's located here: `~/lewagon-workshop/04-databases/02-sql-queries/team_building.sqlite`
+
+Then go to the `Execute SQL` tab, type your query and press ▶ (the play button).
+
+## Git Commit
+
+When you're done, don't forget to **commit and push** your changes.
+
+## Hints
+
+As a reminder, here are the most common SQL keywords and their expected order:
+
+```sql
+SELECT
+FROM
+JOIN
+WHERE
+GROUP BY
+HAVING
+ORDER BY
+LIMIT
+```
+
+Some of them haven't been presented yet. Feel free to search for their documentation.
---
+++ 04-databases/02-sql-queries/queries.sql
@@ -0,0 +1,117 @@
+---------------------------------------
+-- [THE BASICS]
+---------------------------------------
+
+-- Get all sites
+-- TODO: SELECT ...
+
+-- Get all sites sorted by name
+-- TODO: SELECT ...
+
+-- Gett all activity names sorted by name
+-- TODO: SELECT ...
+
+-- Get all "Sport" activities
+-- TODO: SELECT ...
+-- Expected result: 8 rows
+
+-- Get all Escape Games in Nantes
+-- TODO: SELECT ...
+-- Expected result: 3 rows
+
+---------------------------------------
+-- [AGGREGATES]
+---------------------------------------
+
+-- Count all the activities
+-- TODO: SELECT ...
+-- Expected result: 44
+
+-- Count all "Sport" activities
+-- TODO: SELECT ...
+-- Expected result: 13
+
+-- Count the number of activities per category
+-- TODO: SELECT ...
+-- Expected result:
+-- category    total
+-- ----------  ----------
+-- Adventure   13
+-- Cultural    6
+-- Food        6
+-- Sport       8
+-- Visits      5
+-- Workshops   6
+
+-- Get the 3 category names with the biggest number of activities
+-- TODO: SELECT ...
+-- Expected result:
+-- category    total
+-- ----------  ----------
+-- Adventure   13
+-- Sport       8
+-- Cultural    6
+
+---------------------------------------
+-- [JOINS]
+---------------------------------------
+
+-- TODO: SELECT ...
+-- TODO: SELECT ...
+-- Expected result: 53 rows
+
+-- Get all the activity names where you have been to, sorted by name
+-- TODO: SELECT ...
+-- Expected result: well, it depends on who you are :)
+
+-- [NEW KEYWORD] Get all the team names that have done an Adventure activity, sorted by names
+-- TODO: SELECT ...
+-- Expected result:
+-- name
+-- --------------------
+-- Business Development
+-- Product Owner
+-- R&D
+
+-- Get the team names and total number of team building sessions done, sorted by top teams
+-- TODO: SELECT ...
+-- Expected result:
+-- name        total_sessions
+-- ----------  --------------
+-- R&D         7
+-- Marketing   4
+-- Business D  3
+-- Product Ow  3
+-- Finance &   2
+-- UI / UX     2
+-- Customer S  1
+
+-- Get all the employee names, team names and site names that have never attended a team building session
+-- TODO: SELECT ...
+-- Expected result:
+-- first_name  last_name   team        site
+-- ----------  ----------  ----------  -----------
+-- Harmony     Florin      Channels    Paris 13ème
+-- Julia       Ivanets     Channels    Paris 13ème
+-- Pierre      Pellan      Channels    Paris 13ème
+
+-- [NEW AGGREGATE] Get the budget spent on team building sessions per team, sorted by most expensive to leASt expensive
+-- TODO: SELECT ...
+-- Expected result:
+-- name          total_price
+-- ------------  -----------
+-- Finance & RH  1620
+-- Business Dev  1446
+-- R&D           1140
+-- Marketing     445
+-- Customer Suc  360
+-- Product Owne  267
+-- UI / UX       240
+
+-- Get the site names and total number of team building sessions done, sorted by top sites
+-- TODO: SELECT ...
+-- Expected result:
+-- name           total_sessions
+-- -------------  --------------
+-- Ile de Nantes  12
+-- Paris 13ème    10

a4a18916f22d94a41379ec5a0164eaef632bdc90
Morgan Lebois <mlebois@lucca.fr>
2019-11-06 15:29:16 +0000

    -listing program

--- 02-ruby-basics/03-activity-details/details.rb
+++ 02-ruby-basics/03-activity-details/details.rb
@@ -1,5 +1,14 @@
 activity = {
-  # TODO: gather the characteristics of your preferred activity
-}
+  "Name" => "Escape Game",
+  "Category" => "Adventure",
+  "City" => "Limoges",
+  "Duration" => 60,
+  "Number of participants" => 6,
+  "Price" => 15
+  }
 
-# TODO: your code goes here
+  puts "#{activity["Name"]} [#{activity["Category"]}]"
+  puts "Located in #{activity["City"]}"
+  puts "Duration of #{activity["Duration"]} minutes"
+  puts "For #{activity["Number of participants"]} participants"
+  puts "Charged #{activity["Price"]*activity["Number of participants"]}€"
--- 02-ruby-basics/04-listing-activities/listing.rb
+++ 02-ruby-basics/04-listing-activities/listing.rb
@@ -1,5 +1,35 @@
 activities = [
-  # TODO: gather at least 3 activities
+  {
+  "Name" => "Escape Game",
+  "Category" => "Adventure",
+  "City" => "Limoges",
+  "Duration" => 60,
+  "Number of participants" => 6,
+  "Price" => 15
+  },
+  {
+  "Name" => "Museum visit",
+  "Category" => "culture",
+  "City" => "Paris",
+  "Duration" => 120,
+  "Number of participants" => 12,
+  "Price" => 20
+  },
+  {
+  "Name" => "Cinema",
+  "Category" => "Art",
+  "City" => "Limoges",
+  "Duration" => 110,
+  "Number of participants" => 50,
+  "Price" => 12
+    }
 ]
 
-# TODO: your code goes here
+activities.each do |activity|
+  puts "- #{activity["Name"]} [#{activity["Category"].upcase}]"
+  puts "- Located in #{activity["City"]}"
+  puts "- Duration of #{activity["Duration"]} minutes"
+  puts "- For #{activity["Number of participants"]} participants"
+  puts "- Charged #{activity["Price"]*activity["Number of participants"]}€"
+  puts"----"
+end

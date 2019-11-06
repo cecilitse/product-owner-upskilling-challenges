caa6c9c7f38bfa71f1c9c6237035f74b5e8b63a6
Pierre Boucrault <pboucrault@lucca.fr>
2019-11-06 14:56:28 +0000

    Details completed

--- 02-ruby-basics/03-activity-details/details.rb
+++ 02-ruby-basics/03-activity-details/details.rb
@@ -1,5 +1,14 @@
 activity = {
-  # TODO: gather the characteristics of your preferred activity
+  "name" => "Tennis",
+  "category" => "Sport",
+  "city" => "Chouzy-Sur-Cisse",
+  "duration" => 60,
+  "participants_count" => 2,
+  "price_eur" => 23.50
 }
 
-# TODO: your code goes here
+puts "#{activity["name"]} [#{activity["category"].upcase}] \n"
+puts "Located in #{activity["city"]}"
+puts "Duration of #{activity["duration"]} minutes"
+puts "For #{activity["participants_count"]} participants"
+puts "Charged #{activity["price_eur"]}€"

a9e23f89a9a7d666834a40f002e4c03b875fc592
Tristan Goguillot <tgoguillot@lucca.fr>
2019-11-06 15:46:30 +0100

    Complete categories exercise

--- 02-ruby-basics/02-listing-categories/categories.rb
+++ 02-ruby-basics/02-listing-categories/categories.rb
@@ -1,5 +1,23 @@
-categories = [
-  # TODO: gather the categories of activity
-]
+categories = ["Adventure", "Sport", "Visits", "Workshops", "Cultural", "Food"]
 
 # TODO: your code goes here
+
+puts "Our current categories are:"
+
+categories.each do |activity|
+  puts activity
+end
+
+puts
+puts "Please add a category."
+
+new_cat = gets.chomp
+categories << new_cat
+
+puts
+puts "Our categories are:"
+categories.sort.each do |activity|
+  puts "-#{activity}"
+end
+puts
+puts "Total: #{categories.size}."

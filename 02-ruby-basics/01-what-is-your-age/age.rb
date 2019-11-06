276097c326bb5e5bb4da14a7cabdfa2484a4ef0f
Pierre Boucrault <pboucrault@lucca.fr>
2019-11-06 14:13:27 +0000

    What is your age calculation

--- 02-ruby-basics/01-what-is-your-age/age.rb
+++ 02-ruby-basics/01-what-is-your-age/age.rb
@@ -1,6 +1,11 @@
+currentYear = Time.now.year
 puts "What is your name?"
 name = gets.chomp # asks user to type something in the Terminal
-
 puts "Ok got it!"
+puts "Whats is the year of your birth?"
+year = gets.chomp.to_i
+puts "Thank you for that"
 
-# TODO: your code goes here
+age = currentYear - year
+
+puts "Hey #{name}! You are #{age} years old this year."

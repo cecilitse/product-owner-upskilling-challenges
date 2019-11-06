bf65a5b76bbed1c73839276ab327091d61b01227
Tristan Goguillot <tgoguillot@lucca.fr>
2019-11-06 15:29:22 +0100

    Complete age exercise

--- 02-ruby-basics/01-what-is-your-age/age.rb
+++ 02-ruby-basics/01-what-is-your-age/age.rb
@@ -4,3 +4,12 @@ name = gets.chomp # asks user to type something in the Terminal
 puts "Ok got it!"
 
 # TODO: your code goes here
+
+puts "What is your year of birth?"
+year_of_birth = gets.chomp
+
+puts "Thanks!"
+
+age = 2019 - year_of_birth.to_i
+
+puts "Hey #{name}! You are #{age} years old this year. But you seem younger."

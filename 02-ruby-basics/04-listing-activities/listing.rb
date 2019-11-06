8a58a7a7f67a84aa0bd124daf0f5abc7d885bdc2
Sophie Roubin <sroubin@lucca.fr>
2019-11-06 15:27:15 +0000

    list of activities

--- 02-ruby-basics/04-listing-activities/listing.rb
+++ 02-ruby-basics/04-listing-activities/listing.rb
@@ -1,5 +1,29 @@
-activities = [
-  # TODO: gather at least 3 activities
-]
+activities = [{ "name" => "Farniente",
+  "category" => "Relaxing",
+  "city" => "at the beach",
+  "duration" => 600,
+  "participants" => 6,
+  "price" => 0
+},
+{ "name" => "Cocktail",
+  "category" => "Relaxing",
+  "city" => "at the beach",
+  "duration" => 60,
+  "participants" => 6,
+  "price" => 600
+},
+{ "name" => "Swimming",
+  "category" => "Sport",
+  "city" => "at the beach",
+  "duration" => 60,
+  "participants" => 6,
+  "price" => 0
+}]
 
-# TODO: your code goes here
+activities.each do |activity|
+  puts "#{activity["name"]} [#{activity["category"]}]
+Located #{activity["city"]}
+Duration of #{activity["duration"]} minutes
+For #{activity["participants"]} participants
+Charged #{activity["price"]}€"
+end

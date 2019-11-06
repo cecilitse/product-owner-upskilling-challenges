5a2be660a4897f8914e9eabf33873fc188f2a1d1
Morgan Lebois <mlebois@lucca.fr>
2019-11-06 14:34:11 +0000

    What is your age program

--- 02-ruby-basics/01-what-is-your-age/age.rb
+++ 02-ruby-basics/01-what-is-your-age/age.rb
@@ -4,3 +4,10 @@ name = gets.chomp # asks user to type something in the Terminal
 puts "Ok got it!"
 
 # TODO: your code goes here
+
+puts "What is your year of birth?"
+year_of_birth = gets.chomp
+
+age =  2019 - year_of_birth.to_i
+
+puts "Hey #{name.downcase.capitalize}! You are #{age} years old this year."

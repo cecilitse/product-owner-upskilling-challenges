4a43281b33d09a5154753e8728d1f80e6f836c33
Sophie Roubin <sroubin@lucca.fr>
2019-11-06 14:38:19 +0000

    Let me guess your age

--- 02-ruby-basics/01-what-is-your-age/age.rb
+++ 02-ruby-basics/01-what-is-your-age/age.rb
@@ -4,3 +4,11 @@ name = gets.chomp # asks user to type something in the Terminal
 puts "Ok got it!"
 
 # TODO: your code goes here
+puts "What is your year of birth?"
+yob = gets.chomp.to_i
+
+puts "Ok let me guess your age..."
+
+age = 2019 - yob
+
+puts "Hey #{name}! You are #{age} yo this year."

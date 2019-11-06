02a12d253e046cca813740de4715d64ac65b9e6f
Tristan Goguillot <tgoguillot@lucca.fr>
2019-11-06 16:02:07 +0100

    Complete details exercise

--- 02-ruby-basics/03-activity-details/details.rb
+++ 02-ruby-basics/03-activity-details/details.rb
@@ -1,5 +1,17 @@
 activity = {
-  # TODO: gather the characteristics of your preferred activity
+  "name" => "Visite de Nantes",
+  "category" => "Visits",
+  "city" => "Nantes",
+  "duration" => 120,
+  "number_of_participants" => 10,
+  "price" => 100
 }
 
+
 # TODO: your code goes here
+
+puts "#{activity["name"]} [#{activity["category"].upcase}]"
+puts "Located in #{activity["city"]}"
+puts "Duration of #{activity["duration"]} minutes"
+puts "For #{activity["number_of_participants"]} participants"
+puts "Charged #{activity["price"]}€"

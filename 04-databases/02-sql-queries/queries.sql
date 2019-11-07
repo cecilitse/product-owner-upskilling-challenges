17eaa51d1e474eb604ab6beb9566f3f4260c3618
Morgan Lebois <mlebois@lucca.fr>
2019-11-07 13:36:14 +0000

    sql queries exercice

--- 04-databases/02-sql-queries/queries.sql
+++ 04-databases/02-sql-queries/queries.sql
@@ -4,20 +4,28 @@
 
 -- Get all sites
 -- TODO: SELECT ...
+SELECT * FROM sites;
 
 -- Get all sites sorted by name
 -- TODO: SELECT ...
+SELECT * FROM sites ORDER BY name DESC ;
+SELECT * FROM sites ORDER BY name ASC ;
 
 -- Gett all activity names sorted by name
 -- TODO: SELECT ...
+SELECT * FROM activities ORDER BY name;
 
 -- Get all "Sport" activities
 -- TODO: SELECT ...
 -- Expected result: 8 rows
+SELECT * FROM activities
+WHERE category="Sport";
 
 -- Get all Escape Games in Nantes
 -- TODO: SELECT ...
 -- Expected result: 3 rows
+SELECT * FROM activities
+WHERE category="Sport" AND city="Nantes";
 
 ---------------------------------------
 -- [AGGREGATES]
@@ -26,10 +34,14 @@
 -- Count all the activities
 -- TODO: SELECT ...
 -- Expected result: 44
+SELECT COUNT (*) FROM activities ;
 
 -- Count all "Adventure" activities
 -- TODO: SELECT ...
 -- Expected result: 13
+SELECT COUNT (*) FROM activities
+WHERE category="Adventure" ;
+
 
 -- Count the number of activities per category
 -- TODO: SELECT ...
@@ -42,6 +54,10 @@
 -- Sport       8
 -- Visits      5
 -- Workshops   6
+SELECT COUNT (*) as total, category
+FROM activities
+GROUP BY category
+;
 
 -- Get the 3 category names with the biggest number of activities
 -- TODO: SELECT ...
@@ -52,6 +68,14 @@
 -- Sport       8
 -- Cultural    6
 
+SELECT COUNT (*) as total, category
+FROM activities
+GROUP BY category
+ORDER BY total DESC
+LIMIT 3
+;
+
+
 ---------------------------------------
 -- [JOINS]
 ---------------------------------------
@@ -59,10 +83,21 @@
 -- Get all employee names working FROM site "Ile de Nantes", sorted by first name
 -- TODO: SELECT ...
 -- Expected result: 53 rows
+SELECT * from employees INNER JOIN sites ON sites.id = employees.site_id
+WHERE sites.name = "Ile de Nantes"
+ORDER BY employees.first_name
+;
 
 -- Get all the activity names where you have been to, sorted by name
 -- TODO: SELECT ...
 -- Expected result: well, it depends on who you are :)
+SELECT * from activities
+JOIN team_building_sessions ON activities.id = team_building_sessions.activity_id
+JOIN participations ON participations.team_building_session_id = team_building_sessions.id
+JOIN employees ON employees.id = participations.employee_id
+WHERE employees.last_name ="Lebois"
+ORDER BY activities.name
+;
 
 -- [NEW KEYWORD] Get all the team names that have done an Adventure activity, sorted by names
 -- TODO: SELECT ...
@@ -72,6 +107,14 @@
 -- Business Development
 -- Product Owner
 -- R&D
+SELECT DISTINCT teams.name  from teams
+JOIN team_building_sessions ON teams.id = team_building_sessions.team_id
+JOIN activities ON team_building_sessions.activity_id = activities.id
+where activities.category = "Adventure"
+order by teams.name
+;
+
+
 
 -- Get the team names and total number of team building sessions done, sorted by top teams
 -- TODO: SELECT ...
@@ -85,6 +128,12 @@
 -- Finance &   2
 -- UI / UX     2
 -- Customer S  1
+SELECT teams.name, COUNT(team_building_sessions.id) as session_number
+FROM teams
+JOIN team_building_sessions ON teams.id = team_building_sessions.team_id
+GROUP BY teams.name
+ORDER BY session_number DESC
+;
 
 -- Get all the employee names, team names and site names that have never attended a team building session
 -- TODO: SELECT ...
@@ -94,6 +143,12 @@
 -- Harmony     Florin      Channels    Paris 13ème
 -- Julia       Ivanets     Channels    Paris 13ème
 -- Pierre      Pellan      Channels    Paris 13ème
+SELECT employees.first_name, employees.last_name, teams.name as team, sites.name as site FROM employees
+LEFT OUTER JOIN participations ON employees.id = participations.employee_id
+JOIN sites ON employees.site_id = sites.id
+JOIN teams ON teams.id = employees.team_id
+WHERE team_building_session_id is NULL
+;
 
 -- [NEW AGGREGATE] Get the budget spent on team building sessions per team, sorted by most expensive to leASt expensive
 -- TODO: SELECT ...
@@ -107,6 +162,11 @@
 -- Customer Suc  360
 -- Product Owne  267
 -- UI / UX       240
+SELECT  teams.name ,SUM(price) as total_price FROM teams
+JOIN team_building_sessions ON teams.id = team_building_sessions.team_id
+GROUP BY teams.name
+ORDER BY total_price DESC
+;
 
 -- Get the site names and total number of team building sessions done, sorted by top sites
 -- TODO: SELECT ...
@@ -115,3 +175,10 @@
 -- -------------  --------------
 -- Ile de Nantes  12
 -- Paris 13ème    10
+SELECT  sites.name , COUNT(DISTINCT team_building_sessions.id) as total_sessions
+FROM sites
+JOIN employees ON employees.site_id = sites.id
+JOIN participations ON participations.employee_id = employees.id
+JOIN team_building_sessions ON team_building_sessions.id = participations.team_building_session_id
+GROUP BY name
+;

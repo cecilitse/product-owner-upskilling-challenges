188afe885e70aa108e606430a99c39d4d1e3355d
Sophie Roubin <sroubin@lucca.fr>
2019-11-07 08:43:10 +0000

    Premiere version du site

--- 03-sinatra-web-app/app.rb
+++ 03-sinatra-web-app/app.rb
@@ -5,9 +5,30 @@ require "sinatra/reloader" if development?
 enable :static
 
 get "/" do
-  @activities = [
-    # TODO: gather a few activities
-  ]
+  @activities = [{ "name" => "Farniente",
+    "category" => "Relaxing",
+    "city" => "at the beach",
+    "duration" => 600,
+    "participants" => 6,
+    "price" => 0,
+    "image" => "https://www.ithaka.travel/blog/wp-content/uploads/2017/09/Featured-Image-8.jpg"
+  },
+  { "name" => "Cocktail",
+    "category" => "Relaxing",
+    "city" => "at the beach",
+    "duration" => 60,
+    "participants" => 6,
+    "price" => 600,
+    "image" => "https://www.ithaka.travel/blog/wp-content/uploads/2017/09/Featured-Image-8.jpg"
+  },
+  { "name" => "Swimming",
+    "category" => "Sport",
+    "city" => "at the beach",
+    "duration" => 60,
+    "participants" => 6,
+    "price" => 0,
+    "image" => "https://www.ithaka.travel/blog/wp-content/uploads/2017/09/Featured-Image-8.jpg"
+  }]
 
   erb :index
 end

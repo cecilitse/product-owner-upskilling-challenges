140a67c8f594997edf3d018c810c40a716cab3e7
Tristan Goguillot <tgoguillot@lucca.fr>
2019-11-08 13:16:16 +0100

    Add search user exercise

--- 05-api-consumption/01-ruby-program/search_user.rb
+++ 05-api-consumption/01-ruby-program/search_user.rb
@@ -7,3 +7,22 @@ name = gets.chomp # asks user to type something in the Terminal
 puts "Ok got it! Let's search for that person..."
 
 # TODO: your code goes here
+url = "https://wagon.ilucca-demo.net/api/v3/users"
+token = "18afcfb8-697b-4742-b726-418269485cee"
+params = {
+  "name" => "like,#{name}",
+  "fields" => "name, mail, jobTitle, department.name"
+}
+
+response = RestClient.get(url, "params" => params, "Authorization" => "Lucca application=#{token}" )
+
+payload = JSON.parse(response.body)
+
+user_name = payload["data"]["items"][0]["name"]
+user_email = payload["data"]["items"][0]["mail"]
+user_title = payload["data"]["items"][0]["jobTitle"]
+user_department = payload["data"]["items"][0]["department"]["name"]
+
+puts "#{user_name} (#{user_email})"
+puts user_title
+puts "From department #{user_department}"

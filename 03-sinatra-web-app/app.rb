b02911fa96a2ea9335b79b58862250d06939f558
Sophie Roubin <sroubin@lucca.fr>
2019-11-08 17:29:51 +0100

    update photos

--- 03-sinatra-web-app/app.rb
+++ 03-sinatra-web-app/app.rb
@@ -19,16 +19,38 @@ get "/" do
     "duration" => 60,
     "participants" => 6,
     "price" => 600,
-    "image" => "https://www.ithaka.travel/blog/wp-content/uploads/2017/09/Featured-Image-8.jpg"
+    "image" => "https://images.unsplash.com/photo-1556463208-f8c2505e8299"
   },
-  { "name" => "Swimming",
+  { "name" => "Snorkeling",
     "category" => "Sport",
     "city" => "at the beach",
     "duration" => 60,
     "participants" => 6,
-    "price" => 0,
-    "image" => "https://www.ithaka.travel/blog/wp-content/uploads/2017/09/Featured-Image-8.jpg"
+    "price" => 300,
+    "image" => "https://images.unsplash.com/photo-1543037855-00580e109d39"
   }]
 
   erb :index
+end
+get "/activities/1" do
+  @activity = { "name" => "Farniente",
+    "category" => "Relaxing",
+    "city" => "at the beach",
+    "duration" => 600,
+    "participants" => 6,
+    "price" => 0,
+    "image" => "https://www.ithaka.travel/blog/wp-content/uploads/2017/09/Featured-Image-8.jpg"
+  }
+  erb :activity
+end
+get "/activitii/1" do
+  @activity = { "name" => "Farniente",
+    "category" => "Relaxing",
+    "city" => "at the beach",
+    "duration" => 600,
+    "participants" => 6,
+    "price" => 0,
+    "image" => "https://www.ithaka.travel/blog/wp-content/uploads/2017/09/Featured-Image-8.jpg"
+  }
+  erb :activitii
 end

55eb58c67d837b97120bf407aabaa581937a7900
Sophie Roubin <sroubin@lucca.fr>
2019-11-13 17:37:24 +0100

    filters

--- 06-building-api/app.rb
+++ 06-building-api/app.rb
@@ -17,7 +17,52 @@ get "/" do
 end
 
 namespace "/v1" do
-  # TODO: your code goes here
+  get "/activities" do
+    activities = DB.execute("SELECT * FROM activities ORDER BY activities.name ASC;")
+
+    json "activities" => activities
+  end
+
+  get "/activities/:id" do
+    activities = DB.execute("SELECT * FROM activities WHERE activities.id = #{params["id"]};")
+
+    json "activities" => activities
+  end
+end
+
+namespace "/v2" do
+  # get "/activities" do
+  #   activities = DB.execute("SELECT * FROM activities ORDER BY activities.name ASC;")
+
+  #   json "activities" => activities
+  # end
+
+  get "/activities" do
+
+    # if params["city"] && params["category"]
+    #   activities = DB.execute("SELECT * FROM activities WHERE activities.city = '#{params["city"]}' AND activities.category = '#{params["category"]}' ORDER BY activities.name ASC;")
+    # elsif params["city"] && params["search"]
+    #   activities = DB.execute("SELECT * FROM activities WHERE activities.city = '#{params["city"]}' AND activities.name LIKE '%#{params["search"]}%' ORDER BY activities.name ASC;")
+    filters = []
+
+    if params["city"]
+      filters << "city = '#{params["city"]}'"
+    end
+    if params["category"]
+      filters << "category = '#{params["category"]}'"
+    end
+    if params["search"]
+      filters << "name LIKE '%#{params["search"]}%'"
+    end
+
+    if filters.empty?
+      activities = DB.execute("SELECT * FROM activities ORDER BY activities.name ASC;")
+    else
+      activities = DB.execute("SELECT * FROM activities WHERE #{filters.join(" AND ")} ORDER BY activities.name ASC;")
+    end
+
+    json "activities" => activities
+  end
 end
 
 namespace "/doc" do

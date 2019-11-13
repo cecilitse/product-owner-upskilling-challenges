0f7023593e79ee8c1f737f447fd540ff4cad3618
Sophie Roubin <sroubin@lucca.fr>
2019-11-13 12:27:01 +0100

    filtre pour 2 villes

--- 05-api-consumption/02-sinatra-web-app/app.rb
+++ 05-api-consumption/02-sinatra-web-app/app.rb
@@ -8,7 +8,18 @@ require "sinatra/reloader" if development?
 enable :static
 
 get "/" do
-  @activities = # TODO: retrieve all the activities from the API
+  url = "https://team-building-api.cleverapps.io/v2/activities"
+  # url = "https://team-building-api.cleverapps.io/v2/activities?city=#{params["location"]}"
+  response = RestClient.get(url, "params" => {"city" => params["location"] })
+  @activities = JSON.parse(response.body)["activities"]
 
   erb :index
+end
+
+get "/activities/:id" do
+  url = "https://team-building-api.cleverapps.io/v2/activities/#{params["id"]}"
+  response = RestClient.get(url)
+  @activity = JSON.parse(response.body)["activity"]
+
+  erb :activity
 end

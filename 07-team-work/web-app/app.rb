1fb2b27e031db14fa3da18740f8d90f75e8e9555
Pierre Boucrault <pboucrault@lucca.fr>
2019-11-14 15:07:33 +0000

    Index adaptation

--- 07-team-work/web-app/app.rb
+++ 07-team-work/web-app/app.rb
@@ -16,10 +16,29 @@ get "/" do
     api_params["city"] = params["location"]
   end
 
+  if params["category"] && !params["category"].empty?
+    api_params["category"] = params["category"]
+  end
+  @city = params["location"]
+  @category = params["category"]
+
   response   = RestClient.get(url, params: api_params)
   payload    = JSON.parse(response.body)
 
   @activities = payload["activities"]
+
+  url_general = "http://localhost:4567/v2/activities"
+  response_general = RestClient.get(url_general)
+  response_general_parsed = JSON.parse(response_general)
+
+  cities = []
+  categories = []
+  response_general_parsed["activities"].each do |activity_general|
+    cities = cities << activity_general["city"]
+    categories = categories << activity_general["category"]
+  end
+  @cities_filtered = cities.uniq
+  @categories_filtered = categories.uniq
 
   erb :index
 end

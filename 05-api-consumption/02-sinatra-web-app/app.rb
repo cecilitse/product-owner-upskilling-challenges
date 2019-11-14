4e55bc404de036e55eb876d407f3d8ff2bad23bc
Morgan Lebois <mlebois@lucca.fr>
2019-11-14 15:03:48 +0000

    all work from a few days

--- 05-api-consumption/02-sinatra-web-app/app.rb
+++ 05-api-consumption/02-sinatra-web-app/app.rb
@@ -8,7 +8,33 @@ require "sinatra/reloader" if development?
 enable :static
 
 get "/" do
-  @activities = # TODO: retrieve all the activities from the API
+
+url = "https://team-building-api.cleverapps.io/v2/activities"
+city = params["location"]
+response = RestClient.get(url,"params" => {"city" => city})
+
+parsed_response = JSON.parse(response.body)
+@activities = parsed_response["activities"]
+
 
   erb :index
+end
+
+get "/activities/:id" do
+
+  @id=params[:id]
+  url = "https://team-building-api.cleverapps.io/v2/activities/#{@id}"
+  response = RestClient.get(url)
+
+  @activity = JSON.parse(response.body)
+
+  @name = @activity["activity"]["name"]
+  @category = @activity["activity"]["category"]
+  @duration = @activity["activity"]["duration"]
+  @price = @activity["activity"]["price"]
+  @city = @activity["activity"]["city"]
+  @participants = @activity["activity"]["participants_count"]
+  @photo_url = @activity["activity"]["photo_url"]
+
+  erb :details
 end

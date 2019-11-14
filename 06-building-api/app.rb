9dba2a414641ec15533b962d4aa4cc130f7244ef
Morgan Lebois <mlebois@lucca.fr>
2019-11-14 15:03:02 +0000

    all work since a few days

--- 06-building-api/app.rb
+++ 06-building-api/app.rb
@@ -17,7 +17,87 @@ get "/" do
 end
 
 namespace "/v1" do
-  # TODO: your code goes here
+
+  get "/activities" do
+     activities_req_answer = DB.execute("SELECT * FROM activities;")
+
+     json "activities" => activities_req_answer
+
+  end
+
+  get "/activities/:id" do
+    activity_req_answer = DB.execute("SELECT * FROM activities WHERE activities.id = #{params["id"]} ")
+
+    json "activity ID=#{params["id"]}" => activity_req_answer
+
+  end
+
+end
+
+namespace "/v2" do
+
+  get "/activities" do
+    p params
+    if params.empty?
+      activities_req_answer = DB.execute("SELECT * FROM activities")
+
+    elsif params["city"] != nil && params["category"] == nil && params["search"] == nil
+      activities_req_answer = DB.execute("SELECT * FROM activities WHERE activities.city = '#{params["city"]}'")
+
+    elsif params["city"] != nil && params["category"] != nil && params["search"] == nil
+      activities_req_answer = DB.execute(
+        <<~SQL
+          SELECT * FROM activities
+          WHERE activities.category = '#{params["category"]}' AND activities.city = '#{params["city"]}'
+          ORDER BY activities.name
+        SQL
+      )
+
+    elsif params["city"] != nil && params["category"] == nil && params["search"] != nil
+      activities_req_answer = DB.execute(
+        <<~SQL
+          SELECT * FROM activities
+          WHERE activities.name like '%#{params["search"]}%' AND activities.city = '#{params["city"]}'
+          ORDER BY activities.name
+        SQL
+      )
+
+    elsif params["city"] == nil && params["category"] != nil && params["search"] == nil
+      activities_req_answer = DB.execute("SELECT * FROM activities WHERE activities.category = '#{params["category"]}'")
+
+    elsif params["city"] == nil && params["category"] != nil && params["search"] != nil
+      activities_req_answer = DB.execute("SELECT * FROM activities WHERE activities.category = '#{params["category"]}'")
+
+    elsif params["city"] == nil && params["category"] == nil && params["search"] != nil
+      activities_req_answer = DB.execute("SELECT * FROM activities WHERE activities.name like '%#{params["search"]}%'")
+
+    else
+      activities_req_answer = DB.execute(
+        <<~SQL
+          SELECT * FROM activities
+          WHERE activities.category = '#{params["category"]}'
+                AND activities.city = '#{params["city"]}'
+                AND activities.name like '%#{params["search"]}%'
+          ORDER BY activities.name
+        SQL
+      )
+
+
+    end
+
+
+     json "activities" => activities_req_answer
+
+  end
+
+  get "/activities/:id" do
+    activity_req_answer = DB.execute("SELECT * FROM activities WHERE activities.id = #{params["id"]} ")
+    activity = activity_req_answer.first
+    json "activity ID=#{params["id"]}" => activity
+
+
+  end
+
 end
 
 namespace "/doc" do

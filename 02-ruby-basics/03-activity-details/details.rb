659e5c379c278d72d12900d9d6999e019df14f74
mahen-ramnauth <mramnauth@lucca.fr>
2021-04-21 14:27:49 +0000

    details of activity

--- 02-ruby-basics/03-activity-details/details.rb
+++ 02-ruby-basics/03-activity-details/details.rb
@@ -1,5 +1,16 @@
 activity = {
-  # TODO: gather the characteristics of your preferred activity
+            "name" => "Giant Cluedo",
+            "category" => "Cooperation",
+            "city" => "Marseille",
+            "duration" => 120,
+            "participants" => 30,
+            "price" => 200
 }
 
-# TODO: your code goes here
+#Display the information about the activity
+
+puts "#{activity["name"]} [#{activity["category"]}]"
+puts "Located in #{activity["city"]}"
+puts "Duration of #{activity["duration"]} minutes"
+puts "For #{activity["participants"]} participants"
+puts "Charged #{activity["price"]} euros"

16bb2aea0ba3e4cda6392887650d346041c3c474
Mado Poncet <mponcet@lucca.fr>
2021-04-21 14:20:57 +0000

    Add listing activities exercise

--- 02-ruby-basics/04-listing-activities/listing.rb
+++ 02-ruby-basics/04-listing-activities/listing.rb
@@ -1,5 +1,36 @@
 activities = [
   # TODO: gather at least 3 activities
+  {
+    "name" => "Escape Game",
+    "category" => "Adventure",
+    "city" => "Nantes",
+    "duration" => 60,
+    "number of participants" => 4,
+    "price" => 125
+  },
+  {
+    "name" => "Escape Game 2",
+    "category" => "Adventure",
+    "city" => "Paris",
+    "duration" => 60,
+    "number of participants" => 4,
+    "price" => 200
+  },
+  {
+    "name" => "Escape Game 3",
+    "category" => "Adventure",
+    "city" => "Marseille",
+    "duration" => 60,
+    "number of participants" => 4,
+    "price" => 170
+  }
 ]
 
-# TODO: your code goes here
+activities.each do |activity|
+  puts "#{activity["name"]} [#{activity["category"].upcase}]"
+  puts "Located in #{activity["city"]}"
+  puts "Duration of #{activity["duration"]} minutes"
+  puts "For #{activity["number of participants"]} participants"
+  puts "Charged #{activity["price"]}€"
+  puts "---"
+end

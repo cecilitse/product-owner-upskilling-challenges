b121e56eb31a0c17f69cc3b9950c01b053a54788
mahen-ramnauth <mramnauth@lucca.fr>
2021-04-21 13:37:35 +0000

    Age of the user

--- 02-ruby-basics/01-what-is-your-age/age.rb
+++ 02-ruby-basics/01-what-is-your-age/age.rb
@@ -1,6 +1,13 @@
+#Get the name of the user
 puts "What is your name?"
 name = gets.chomp # asks user to type something in the Terminal
 
-puts "Ok got it!"
+#Get the year of birth of the user
+puts "What is your year of birth ?"
+yearofbirth = gets.chomp
 
-# TODO: your code goes here
+#Compute the age of the person
+age=2021-yearofbirth.to_i
+
+#Display the name and age of the person
+puts "Hey #{name} ! You are #{age} years old this year"

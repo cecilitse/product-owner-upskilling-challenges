6f25c0068c5533a3804b611ab82bfc3d8903ca52
mahen-ramnauth <mramnauth@lucca.fr>
2021-04-22 12:27:52 +0000

    save app

--- 03-sinatra-web-app/app.rb
+++ 03-sinatra-web-app/app.rb
@@ -6,7 +6,30 @@ enable :static
 
 get "/" do
   @activities = [
-    # TODO: gather a few activities
+      {
+        "name" => "Giant Cluedo",
+        "category" => "Cooperation",
+        "city" => "Marseille",
+        "duration" => 120,
+        "participants" => 30,
+        "price" => 200
+        },
+      {
+        "name" => "Surf",
+        "category" => "Sport",
+        "city" => "Hossegor",
+        "duration" => 180,
+        "participants" => 20,
+        "price" => 250
+        },
+      {
+        "name" => "Hockey",
+        "category" => "Sport",
+        "city" => "Paris",
+        "duration" => 90,
+        "participants" => 20,
+        "price" => 150
+        },
   ]
 
   erb :index

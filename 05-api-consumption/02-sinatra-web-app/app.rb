855323a66497cd227c6cf839ee9789a8a6c9315c
mahen-ramnauth <mramnauth@lucca.fr>
2021-04-23 12:05:58 +0000

    save website team_building

--- 05-api-consumption/02-sinatra-web-app/app.rb
+++ 05-api-consumption/02-sinatra-web-app/app.rb
@@ -8,9 +8,23 @@ require "sinatra/reloader" if development?
 enable :static
 
 get "/" do
-  @activities = # TODO: retrieve all the activities from the API
+  activities_url = "https://team-building-api.cleverapps.io/v2/activities"
+  response = RestClient.get(activities_url,params: {"city" => params["location"]})
+  payload = JSON.parse(response.body)
+  @activities = payload["activities"]
+
 
   erb :index
+end
+
+get "/activities/:id" do
+  id= params["id"]
+  activity_url = "https://team-building-api.cleverapps.io/v2/activities/#{id}"
+  response = RestClient.get(activity_url)
+  payload = JSON.parse(response.body)
+  @activity = payload["activity"]
+
+  erb :show
 end
 
 get "/components" do

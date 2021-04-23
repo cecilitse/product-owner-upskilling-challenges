5ec1d2d38286a15e2f1ec664a836b3d1879ea44f
Mado Poncet <mponcet@lucca.fr>
2021-04-23 10:18:05 +0000

    Add consuming API from Sinatra exercise

--- 05-api-consumption/02-sinatra-web-app/app.rb
+++ 05-api-consumption/02-sinatra-web-app/app.rb
@@ -8,9 +8,20 @@ require "sinatra/reloader" if development?
 enable :static
 
 get "/" do
-  @activities = # TODO: retrieve all the activities from the API
+  activities_url = "https://team-building-api.cleverapps.io/v2/activities"
+  response = RestClient.get(activities_url, params: { "city" => params["location"]})
+  activities_collection = JSON.parse(response.body)
+  @activities = activities_collection["activities"]
 
   erb :index
+end
+
+get "/activities/:id" do
+  activity_url = "https://team-building-api.cleverapps.io/v2/activities/#{params["id"]}"
+  response = RestClient.get(activity_url)
+  @activity = JSON.parse(response.body)["activity"]
+
+  erb :index_activity
 end
 
 get "/components" do

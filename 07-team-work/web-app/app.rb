b6fd4a449b4dd010b4c8897d0cae3e865183fa4e
mahen-ramnauth <mramnauth@lucca.fr>
2021-05-05 15:58:57 +0000

    add button to WL to choose your site + modification sites in app.rb

--- 07-team-work/web-app/app.rb
+++ 07-team-work/web-app/app.rb
@@ -33,16 +33,16 @@ get "/activities/:id" do
   @activity = payload["activity"]
 
   url_sites     = "http://localhost:4567/v2/sites"
-  response_sites = RestClient.get(url)
+  response_sites = RestClient.get(url_sites)
   payload_sites  = JSON.parse(response_sites.body)
 
-  @site = payload_sites["site"]
+  @sites = payload_sites["sites"]
 
-  url_favorites     = "http://localhost:4567/v2/favorites"
-  response_favorites = RestClient.get(url)
-  payload_favorites  = JSON.parse(response_favorites.body)
+  # url_favorites     = "http://localhost:4567/v2/favorites"
+  # response_favorites = RestClient.get(url_favorites)
+  # payload_favorites  = JSON.parse(response_favorites.body)
 
-  @favorite = payload_favorites["favorite"]
+  # @favorites = payload_favorites["favorites"]
 
   erb :show
 end

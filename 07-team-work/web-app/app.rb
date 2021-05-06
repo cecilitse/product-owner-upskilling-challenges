1fc86089a7572865fab2fbb47f9fc60b11770e30
mahen-ramnauth <mramnauth@lucca.fr>
2021-05-06 11:51:58 +0000

    add favorites in sql base, add api get and post, add post in app.rb, add checkboxes in show.erb

--- 07-team-work/web-app/app.rb
+++ 07-team-work/web-app/app.rb
@@ -38,13 +38,43 @@ get "/activities/:id" do
 
   @sites = payload_sites["sites"]
 
-  # url_favorites     = "http://localhost:4567/v2/favorites"
-  # response_favorites = RestClient.get(url_favorites)
-  # payload_favorites  = JSON.parse(response_favorites.body)
+  url_favorites     = "http://localhost:4567/v2/favorites"
+  response_favorites = RestClient.get(url_favorites)
+  payload_favorites  = JSON.parse(response_favorites.body)
 
-  # @favorites = payload_favorites["favorites"]
+  @favorites = payload_favorites["favorites"]
 
   erb :show
+end
+
+post "/favorites" do
+  activity_id = params["activity_id"].to_i
+  site_ids = params["site_ids"]
+
+  #GET
+  url_activity_favorites      = "http://localhost:4567/v2/favorites"
+  response_activity_favorites = RestClient.get(url_activity_favorites, params: {"activity_id" => activity_id})
+  payload_activity_favorites  = JSON.parse(response_activity_favorites.body)
+
+  @activity_favorites = payload_activity_favorites["favorites"]
+  site_ids_test=[]
+  @activity_favorites.each do |favorite|
+    site_ids_test << favorite["site_id"]
+  end
+
+  #POST
+  site_ids.each do |site_id|
+    url_post_favorites     = "http://localhost:4567/v2/favorites"
+    headers = { content_type: "json" }
+
+    favorites_params = {
+        "activity_id" => activity_id,
+        "site_id"  => site_id.to_i
+    }
+
+    response_post_favorites = RestClient.post(url_post_favorites, favorites_params.to_json, headers)
+  end
+  redirect to("/activities/#{activity_id}")
 end
 
 get "/components" do
